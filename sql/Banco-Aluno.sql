cbde4cdb27ff64ed7009cef7f01dc14c184cfc83
thalesgilbertosilva <thalesgilberto.silva@gmail.com>
2018-07-22 04:22:38 -0300

    CRUD finalizado!

--- sql/Banco-Aluno.sql
+++ sql/Banco-Aluno.sql
@@ -4,7 +4,7 @@ USE `aluno`;
 --
 -- Host: 127.0.0.1    Database: aluno
 -- ------------------------------------------------------
--- Server version	5.5.5-10.1.26-MariaDB
+-- Server version	5.5.5-10.1.25-MariaDB
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -35,18 +35,8 @@ CREATE TABLE `alunos` (
   `created_at` timestamp NULL DEFAULT NULL,
   `updated_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `alunos`
---
-
-LOCK TABLES `alunos` WRITE;
-/*!40000 ALTER TABLE `alunos` DISABLE KEYS */;
-INSERT INTO `alunos` VALUES (1,'Thales Gilberto Celestino da Silva','00000000000','M','1998-09-11','Rua Tal','thalesgilberto.silva@gmail.com','2018-07-22 02:50:31','2018-07-22 02:50:31'),(2,'Thales Gilberto','00000000000','M','1998-09-11','Rua Tal','thalesgilbertosilva@gmail.com','2018-07-22 04:12:06','2018-07-22 04:12:06'),(3,'Keyla Alves','22222222222','F','1996-12-17','Rua Thales meu Amor','keyla@gmail.com','2018-07-22 04:18:58','2018-07-22 04:18:58'),(4,'Vitor Celestino','44444444444','M','1999-12-27','Rua Tal','vitor@gmail.com','2018-07-22 04:22:13','2018-07-22 04:22:13'),(5,'Juliana Miranda Celestino da Silva','55555555555','F','1979-05-16','Rua Tal','juliana@gmail.com','2018-07-22 04:30:56','2018-07-22 04:30:56'),(6,'Juliana Miranda Celestino da Silva','55555555555','F','1979-05-16','Rua Tal','juliana@gmail.com','2018-07-22 04:31:55','2018-07-22 04:31:55');
-/*!40000 ALTER TABLE `alunos` ENABLE KEYS */;
-UNLOCK TABLES;
 
 --
 -- Table structure for table `migrations`
@@ -62,16 +52,6 @@ CREATE TABLE `migrations` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `migrations`
---
-
-LOCK TABLES `migrations` WRITE;
-/*!40000 ALTER TABLE `migrations` DISABLE KEYS */;
-INSERT INTO `migrations` VALUES (1,'2018_07_21_233307_create_aluno_table',1),(2,'2018_07_21_234644_create_alunos_table',2);
-/*!40000 ALTER TABLE `migrations` ENABLE KEYS */;
-UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -82,4 +62,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-07-21 23:36:27
+-- Dump completed on 2018-07-22  4:18:53
